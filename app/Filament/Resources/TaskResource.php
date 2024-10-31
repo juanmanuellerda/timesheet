35ee853f6b6fdb3ce3c27bc49ea8e2de497d8cea
Juan Manuel <juanmanuellerda@gmail.com>
2024-10-31 15:31:05 -0300

    cambie los iconos users y task

--- app/Filament/Resources/TaskResource.php
+++ app/Filament/Resources/TaskResource.php
@@ -17,7 +17,7 @@ class TaskResource extends Resource
 {
     protected static ?string $model = Task::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-archive-box-arrow-down';
+    protected static ?string $navigationIcon = 'heroicon-s-wrench-screwdriver';
 
     public static function form(Form $form): Form
     {

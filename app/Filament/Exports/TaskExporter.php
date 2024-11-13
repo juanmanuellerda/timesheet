2c1babb47b3623772d380444ba2ee42c886d6625
Juan Manuel <juanmanuellerda@gmail.com>
2024-11-13 07:57:02 -0300

    se crearon tablas para poder exportar planilla excel, se cargo la clase taskexport con los registros exportar, se agrego la notificación de aviso de exportación

---
+++ app/Filament/Exports/TaskExporter.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Filament\Exports;
+
+use App\Models\Task;
+use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
+use Filament\Actions\Exports\Models\Export;
+
+class TaskExporter extends Exporter
+{
+    protected static ?string $model = Task::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ExportColumn::make('id')
+                ->label('ID'),
+            ExportColumn::make('project.name'),
+            ExportColumn::make('external_id'),
+            ExportColumn::make('name'),
+            ExportColumn::make('comment'),
+            ExportColumn::make('file'),
+            ExportColumn::make('date'),
+            ExportColumn::make('duration'),
+            ExportColumn::make('created_at'),
+            ExportColumn::make('updated_at'),
+        ];
+    }
+
+    public static function getCompletedNotificationBody(Export $export): string
+    {
+        $body = 'Your task export has completed and ' . number_format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+
+        if ($failedRowsCount = $export->getFailedRowsCount()) {
+            $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
+        }
+
+        return $body;
+    }
+}

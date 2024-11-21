9a684088cda178f20f949c385aa1bc51d19d1699
Juan Manuel <juanmanuellerda@gmail.com>
2024-11-21 15:46:22 -0300

    cree tabla activities y le agregue posteriormente la columna status

---
+++ database/migrations/2024_11_21_122706_create_activities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('task_id');
+            $table->foreign('task_id')
+                ->references('id')
+                ->on('tasks')
+                ->cascadeOnDelete();  
+            $table->string('name');
+            $table->timestamp('from_date')
+                ->nullable();
+            $table->timestamp('to_date')
+                ->nullable();
+            $table->text('comment')
+                ->nullable();            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};

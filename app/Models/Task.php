9c0b5e579e7094b53fc4896451f507ceed791bd5
Juan Manuel <juanmanuellerda@gmail.com>
2024-11-21 15:48:49 -0300

    agregue relacion con activities

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -27,5 +27,9 @@ class Task extends Model
         return $this->belongsTo(Project::class);
     } 
 
+    public function activity(): BelongsTo {
+        return $this->belongsTo(Activity::class);
+    } 
+
 }
 

3c14799406c1e5857b69f8b0e15b7882afa3edeb
Juan Manuel <juanmanuellerda@gmail.com>
2024-11-28 15:08:25 -0300

    código Sandro: devuelve la conusltas SQL ejecutadas

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
-
-
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         model::unguard();
+        DB::listen(function ($event) {
+            info($event->toRawSql());
+        });
     }
 }

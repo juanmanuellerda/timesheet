9faf9255ab636a9a19f411f797a1e9c16a13d79f
Juan Manuel <juanmanuellerda@gmail.com>
2024-11-28 15:14:18 -0300

    código Sandro: accessor "isCompleted", no necesita almacenarse en la base de datos. Lo uso en lugar de booleano "status" que estaba definido en la base de datos

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Activity extends Model
 {
@@ -15,6 +15,12 @@ class Activity extends Model
         return $this->belongsTo(Task::class);
     } 
 
+    protected function isCompleted(): Attribute 
+    {
     
+    return Attribute::make(
+        get: fn (mixed $value) => (bool) $this->end_at,
+        );
+    }
 }
 
